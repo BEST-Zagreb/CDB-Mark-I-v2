be8d9408c2a7c5e36d4b4e845f468e79c1454aa4
CrazyFreak <jakovbla4@gmail.com>
2025-11-02 20:48:31 +0100

    Added JJ to not found as easter egg #2 Improved resolution on logo on home page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,10 +5,12 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import Image from "next/image";
 
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
@@ -19,43 +21,59 @@ export default function NotFound() {
 
   return (
     <div className="m-2 flex items-center justify-center min-h-[70dvh]">
-      <Card className="w-full max-w-md mx-auto">
-        <CardHeader className="text-center space-y-4 text-muted-foreground">
-          <CardTitle>
-            <div className="text-6xl font-bold">404</div>
+      <Card className="w-full max-w-lg mx-auto">
+        <div className="flex flex-wrap sm:flex-nowrap items-center justify-center gap-6 sm:gap-2">
+          <div className="">
+            <Image
+              src="/JJ_transparent.png"
+              alt="JJ"
+              width={512}
+              height={512}
+              className="w-32 sm:w-48"
+            />
+          </div>
 
-            <div className="text-2xl">Hmm… well this is awkward. 😅</div>
-          </CardTitle>
+          <div>
+            <CardHeader className="text-center space-y-4">
+              <CardTitle>
+                <div className="text-3xl sm:text-6xl font-bold">404</div>
 
-          <CardDescription className="space-y-2">
-            <div>
-              It seems that the page{" "}
-              <span className="italic"> &quot;{pathname}&quot; </span> does not
-              exist.
-            </div>
+                <div className="text-lg sm:text-2xl">
+                  Hmm… well this is awkward. 😅
+                </div>
+              </CardTitle>
 
-            <div className="text-gray-600">
-              Let&apos;s get you back on track!
-            </div>
-          </CardDescription>
-        </CardHeader>
+              <CardDescription className="space-y-2">
+                <div>
+                  It seems that the page{" "}
+                  <span className="italic"> &quot;{pathname}&quot; </span> does
+                  not exist.
+                </div>
 
-        <CardContent className="text-center mt-2 flex flex-wrap gap-4 items-center justify-center">
-          <Button
-            variant={"outline"}
-            onClick={() => {
-              router.back();
-            }}
-          >
-            Go back
-          </Button>
+                <div className="text-gray-600">
+                  Let&apos;s get you back on track!
+                </div>
+              </CardDescription>
+            </CardHeader>
 
-          <Button asChild className="text-pretty whitespace-wrap">
-            <Link href="/" className="text-pretty whitespace-wrap">
-              Go to Homepage
-            </Link>
-          </Button>
-        </CardContent>
+            <CardContent className="text-center mt-2 flex flex-wrap gap-4 items-center justify-center">
+              <Button
+                variant={"outline"}
+                onClick={() => {
+                  router.back();
+                }}
+              >
+                Go back
+              </Button>
+
+              <Button asChild className="text-pretty whitespace-wrap">
+                <Link href="/" className="text-pretty whitespace-wrap">
+                  Go to Homepage
+                </Link>
+              </Button>
+            </CardContent>
+          </div>
+        </div>
       </Card>
     </div>
   );
